59cfb75a588f24fc20a39cb1f1ca9040f12d8a00
Francois-Rene Rideau <fare@tunes.org>
2025-10-04 09:54:55 -0400

    reformat mini-coop

--- mini-coop.rkt
+++ mini-coop.rkt
@@ -1,7 +1,8 @@
 #lang racket
-
 ;; style: stateful Y, multiple arities with care, unit value for top
 
+(define-syntax test
+  (syntax-rules () ((_ x) (begin (display 'x) (display " ⇒ ") (display x) (newline)))))
 (define (Y f) (letrec ((fp (lambda (x) (f fp x)))) fp))
 (define ⊤ (void))
 
@@ -13,28 +14,20 @@
   (case i ((x) 2) ((y) 4) (else super)))
 (define ((color-spec self i) super)
   (case i ((color) 'blue) (else super)))
-
 (define point-p (instantiate
   (inherit coord-spec color-spec)))
-
-(define-syntax test
-  (syntax-rules () ((_ x) (begin (display 'x) (display " ⇒ ") (display x) (newline)))))
-
 (test (point-p 'x)) ;; 2
 (test (point-p 'color)) ;; ⇒ blue
 
 (define (((add-x-spec dx) self i) super)
   (case i ((x) (+ dx super))
           (else super)))
-
 (define ((rho-spec self i) super)
   (case i ((rho)
            (sqrt (+ (sqr (self 'x))
                     (sqr (self 'y)))))
           (else super)))
-
 (define point-r (instantiate
                    (inherit rho-spec (inherit (add-x-spec 1) coord-spec))))
-
 (test (point-r 'x)) ;; ⇒ 3
 (test (point-r 'rho)) ;; ⇒ 5
